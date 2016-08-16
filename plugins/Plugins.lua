1a9cd6633b4dbc21475ad349879f27905b85d5f8
zoner-tm <www.zonerteam@gmail.com>
2016-08-16 18:14:56 +0430

    Update Plugins.lua

--- plugins/Plugins.lua
+++ plugins/Plugins.lua
@@ -25,18 +25,18 @@ local function list_all_plugins(only_enabled)
   local text = ''
   local nsum = 0
   for k, v in pairs( plugins_names( )) do
-    --  🔊️ enabled, 🔇 disabled
-    local status = '🔇'
+    --   enabled, 🔇 disabled
+    local status = '🔴'
     nsum = nsum+1
     nact = 0
     -- Check if is enabled
     for k2, v2 in pairs(_config.enabled_plugins) do
       if v == v2..'.lua' then 
-        status = '🔊️' 
+        status = '🔵' 
       end
       nact = nact+1
     end
-    if not only_enabled or status == '🔊️' then
+    if not only_enabled or status == '🔵' then
       -- get the name
       v = string.match (v, "(.*)%.lua")
       text = text..nsum..'. '..v..'  '..status..'\n'
@@ -51,17 +51,17 @@ local function list_plugins(only_enabled)
   local nsum = 0
   for k, v in pairs( plugins_names( )) do
     --  🔊️ enabled, 🔇 disabled
-    local status = '🔇'
+    local status = '🔴'
     nsum = nsum+1
     nact = 0
     -- Check if is enabled
     for k2, v2 in pairs(_config.enabled_plugins) do
       if v == v2..'.lua' then 
-        status = '🔊️' 
+        status = '🔵' 
       end
       nact = nact+1
     end
-    if not only_enabled or status == '🔊️' then
+    if not only_enabled or status == '🔵' then
       -- get the name
       v = string.match (v, "(.*)%.lua")
       text = text..v..'  '..status..'\n'
